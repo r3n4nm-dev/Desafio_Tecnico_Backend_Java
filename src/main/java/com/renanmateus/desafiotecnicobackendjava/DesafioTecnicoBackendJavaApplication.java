a9a2414318dcdea85f8b19112a97f6df475de47f
r3n4nm-dev <r3n4nm@gmail.com>
2021-07-23 12:32:55 -0300

    Criacao do projeto

---
+++ src/main/java/com/renanmateus/desafiotecnicobackendjava/DesafioTecnicoBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.renanmateus.desafiotecnicobackendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoBackendJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renanmateus/desafiotecnicobackendjava/DesafioTecnicoBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanmateus.desafiotecnicobackendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTecnicoBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
